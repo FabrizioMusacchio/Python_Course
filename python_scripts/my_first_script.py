c816f8d567794d9aba05c8f6d7b96027a19b81eb
Fabrizio Musacchio <fabrizio.musacchio@posteo.de>
2021-07-23 23:29:13 +0200

    Renamed chapters according to the course website chapter counting. Implemented and changed the "Toggle Solution" and "Your solution here" views

---
+++ python_scripts/my_first_script.py
@@ -0,0 +1,35 @@
+#%% My first code cell 
+a=5
+b=20
+print(a+b)
+print(b**a)
+
+#%% My second code cell
+text_A = "Hello, my name is "
+text_B = "Gandalf."
+print(text_A)
+print(text_B)
+print(text_A + text_B)
+
+#%% Line Comments
+# This is a line comment placed at the line beginning:
+print(f"Some text")
+
+# Line comments can be written one after another
+# and as many as you want to
+
+print(f"Some more text")  # they can be arbitrarily placed
+
+#%% Block Comments
+""" This is a block comment """
+print(f"99+1 equals {99+1}")
+
+
+""" You can insert line breaks into block comment, which 
+    useful e.g., for longer comments, as you
+    don't have to write # for each new line.
+"""
+
+print(f"my modd: :-D")
+
+
